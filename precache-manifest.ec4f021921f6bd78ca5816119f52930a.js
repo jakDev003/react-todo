b0730082c55c85e8f7a88902822e1b3bcf8dad3e
Josh <jakdev003@gmail.com>
2020-06-22 17:41:12 -0400

    Updates

---
+++ precache-manifest.ec4f021921f6bd78ca5816119f52930a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd0510a8db7ba25cb5ce67fd80c8bef",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "e182f09227e3366a2fa9",
+    "url": "/react-todo/static/css/main.b007b9be.chunk.css"
+  },
+  {
+    "revision": "2948da7d09ee76d40d67",
+    "url": "/react-todo/static/js/2.0cbf6e38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo/static/js/2.0cbf6e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e182f09227e3366a2fa9",
+    "url": "/react-todo/static/js/main.65b00299.chunk.js"
+  },
+  {
+    "revision": "34ecee2032ea15e07033",
+    "url": "/react-todo/static/js/runtime-main.6fd824dc.js"
+  }
+]);
